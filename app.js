2db81c98eecf92a096b4d783ebd9581d6b6ac1fc
cchen <1120243698@bit.edu.cn>
2025-05-14 15:55:45 +0800

    hot fix

--- app.js
+++ app.js
@@ -111,6 +111,6 @@ let server = app.listen(8080, () => {
     let host = server.address().address;
     let port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://localhost:%s', host, port);
 });
 
